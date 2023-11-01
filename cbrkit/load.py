67d39a52fb6555553c069579fbfef6ed07c17add
Mirko Lenz <mirko@mirkolenz.com>
2023-11-01 23:07:16 +0100

    refactor: sort load methods lexically

--- cbrkit/load.py
+++ cbrkit/load.py
@@ -8,11 +8,21 @@ import yaml
 from cbrkit import model
 
 
-def from_csv(path: model.FilePath) -> model.Casebase[model.CaseType]:
+def load_csv(path: model.FilePath) -> model.Casebase[model.CaseType]:
     return pd.read_csv(path).to_dict(orient="index")
 
 
-def from_yaml(path: model.FilePath) -> model.Casebase[model.CaseType]:
+def load_json(path: model.FilePath) -> model.Casebase[model.CaseType]:
+    with open(path, "rb") as fp:
+        return json.load(fp)
+
+
+def load_toml(path: model.FilePath) -> model.Casebase[model.CaseType]:
+    with open(path, "rb") as fp:
+        return cast(model.Casebase[model.CaseType], tomllib.load(fp))
+
+
+def load_yaml(path: model.FilePath) -> model.Casebase[model.CaseType]:
     data = {}
 
     with open(path, "rb") as fp:
@@ -22,22 +32,12 @@ def from_yaml(path: model.FilePath) -> model.Casebase[model.CaseType]:
     return data
 
 
-def from_toml(path: model.FilePath) -> model.Casebase[model.CaseType]:
-    with open(path, "rb") as fp:
-        return cast(model.Casebase[model.CaseType], tomllib.load(fp))
-
-
-def from_json(path: model.FilePath) -> model.Casebase[model.CaseType]:
-    with open(path, "rb") as fp:
-        return json.load(fp)
-
-
 _mapping: dict[model.LoadFormat, model.LoadFunc] = {
-    "csv": from_csv,
-    "yaml": from_yaml,
-    "yml": from_yaml,
-    "toml": from_toml,
-    "json": from_json,
+    "csv": load_csv,
+    "json": load_json,
+    "toml": load_toml,
+    "yaml": load_yaml,
+    "yml": load_yaml,
 }
 
 
