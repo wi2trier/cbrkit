36a4b644bb54d75397c4c07e87900948b88139e3
Mirko Lenz <mirko@mirkolenz.com>
2023-11-02 23:31:47 +0100

    feat: update loaders, add generic pandas support

--- cbrkit/load.py
+++ cbrkit/load.py
@@ -1,45 +1,70 @@
-import json
-from typing import cast
-
-import pandas as pd
 import tomllib
+from collections import abc
+from pathlib import Path
+from typing import Any, Hashable, Iterator, cast
+
+import orjson as json
+import pandas as pd
 import yaml
+from pandas import DataFrame, Series
 
 from cbrkit import model
 
-
-def load_csv(path: model.FilePath) -> model.Casebase[model.CaseType]:
-    return pd.read_csv(path).to_dict(orient="index")
+__all__ = ("load_file", "load_dataframe")
 
 
-def load_json(path: model.FilePath) -> model.Casebase[model.CaseType]:
+class DataFrameCasebase(abc.Mapping[model.CaseName, model.CaseType]):
+    df: DataFrame
+
+    def __init__(self, df: DataFrame) -> None:
+        self.df = df
+
+    def __getitem__(self, key: int) -> Series:
+        return self.df.iloc[key]
+
+    def __iter__(self) -> Iterator[Hashable]:
+        return iter(self.df.index)
+
+    def __len__(self) -> int:
+        return len(self.df)
+
+
+def load_dataframe(df: DataFrame) -> model.Casebase[pd.Series]:
+    return DataFrameCasebase(df)
+
+
+def _load_json(path: model.FilePath) -> model.Casebase[Any]:
     with open(path, "rb") as fp:
-        return json.load(fp)
+        return json.loads(fp.read())
 
 
-def load_toml(path: model.FilePath) -> model.Casebase[model.CaseType]:
+def _load_toml(path: model.FilePath) -> model.Casebase[Any]:
     with open(path, "rb") as fp:
-        return cast(model.Casebase[model.CaseType], tomllib.load(fp))
+        return cast(model.Casebase, tomllib.load(fp))
 
 
-def load_yaml(path: model.FilePath) -> model.Casebase[model.CaseType]:
-    data = {}
+def _load_yaml(path: model.FilePath) -> model.Casebase[Any]:
+    data: dict[str, Any] = {}
 
     with open(path, "rb") as fp:
         for doc in yaml.safe_load_all(fp):
             data |= doc
 
-    return data
+    return cast(model.Casebase, data)
 
 
-_mapping: dict[model.LoadFormat, model.LoadFunc] = {
-    ".csv": load_csv,
-    ".json": load_json,
-    ".toml": load_toml,
-    ".yaml": load_yaml,
-    ".yml": load_yaml,
+_mapping: dict[model.LoadFormat, model.LoadFunc[Any]] = {
+    ".json": _load_json,
+    ".toml": _load_toml,
+    ".yaml": _load_yaml,
+    ".yml": _load_yaml,
 }
 
 
-def get(name: model.LoadFormat) -> model.LoadFunc:
-    return _mapping[name]
+def load_file(path: model.FilePath) -> model.Casebase[Any]:
+    if isinstance(path, str):
+        path = Path(path)
+
+    format = cast(model.LoadFormat, path.suffix)
+
+    return _mapping[format](path)
