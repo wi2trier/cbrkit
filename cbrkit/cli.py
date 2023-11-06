fb3cac099132d0dea16a0b3811111e52b55a7977
Mirko Lenz <mirko@mirkolenz.com>
2023-11-06 22:15:34 +0100

    fix: update cli

--- cbrkit/cli.py
+++ cbrkit/cli.py
@@ -10,16 +10,16 @@ app = Typer()
 
 @app.command()
 def retrieve(path: Path):
-    casebase = cbrkit.load_path(path)
+    casebase: cbrkit.model.Casebase[dict[str, dict[str, str]]] = cbrkit.load_path(path)
 
     result = cbrkit.retrieve(
         casebase,
-        query=casebase[0],
+        query=casebase["42"],
         similarity_func="equality",
         casebase_limit=5,
     )
 
-    print(result)
+    print(result.ranking)
 
 
 if __name__ == "__main__":
