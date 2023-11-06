ad081687170ddcace716cfe143946aeccae9d8ab
Mirko Lenz <mirko@mirkolenz.com>
2023-11-06 22:15:25 +0100

    fix: use str as casename type

--- cbrkit/model.py
+++ cbrkit/model.py
@@ -10,7 +10,7 @@ from typing import (
 FilePath = str | Path
 
 CaseType = TypeVar("CaseType")
-CaseName = Hashable
+CaseName = str
 Casebase = Mapping[CaseName, CaseType]
 
 SimilarityValue = float
