438cb3ad572d2b08a4a6a8a4c38ab6d05d6e5b2e
Mirko Lenz <mirko@mirkolenz.com>
2023-12-11 20:46:26 +0100

    feat: add transformers and openai similarity

--- cbrkit/sim/strings.py
+++ cbrkit/sim/strings.py
@@ -2,6 +2,7 @@ import csv
 import itertools
 from collections.abc import Sequence
 from pathlib import Path
+from typing import cast
 
 from cbrkit.sim._taxonomy import Taxonomy, TaxonomyMeasure
 from cbrkit.sim.generic import table as generic_table
@@ -14,18 +15,66 @@ from cbrkit.typing import (
 )
 
 
-def spacy(model_name: str = "en_core_web_lg") -> SimSeqFunc[str]:
-    import spacy
+def _cosine(u, v) -> float:
+    import numpy as np
+    import scipy.spatial.distance as scipy_dist
 
-    nlp = spacy.load(model_name)
+    if np.any(u) and np.any(v):
+        return cast(float, 1 - scipy_dist._cosine(u, v))
+
+    return 0.0
+
+
+def _unique_items(pairs: Sequence[tuple[str, str]]) -> list[str]:
+    return [*{*itertools.chain.from_iterable(pairs)}]
+
+
+def spacy(model_name: str = "en_core_web_lg") -> SimSeqFunc[str]:
+    from spacy import load as spacy_load
+
+    nlp = spacy_load(model_name)
 
     def wrapped_func(pairs: Sequence[tuple[str, str]]) -> SimSeq:
-        texts = set(itertools.chain.from_iterable(pairs))
+        texts = _unique_items(pairs)
 
         with nlp.select_pipes(enable=[]):
-            docs = dict(zip(texts, nlp.pipe(texts), strict=True))
+            _docs = nlp.pipe(texts)
+
+        docs = dict(zip(texts, _docs, strict=True))
 
         return [docs[x].similarity(docs[y]) for x, y in pairs]
+
+    return wrapped_func
+
+
+def sentence_transformers(model_name: str) -> SimSeqFunc[str]:
+    from sentence_transformers import SentenceTransformer
+
+    model = SentenceTransformer(model_name)
+
+    def wrapped_func(pairs: Sequence[tuple[str, str]]) -> SimSeq:
+        texts = _unique_items(pairs)
+        _vecs = model.encode(texts, convert_to_numpy=True)
+        vecs = dict(zip(texts, _vecs, strict=True))
+
+        return [_cosine(vecs[x], vecs[y]) for x, y in pairs]
+
+    return wrapped_func
+
+
+def openai(model_name: str) -> SimSeqFunc[str]:
+    import numpy as np
+    from openai import Client
+
+    client = Client()
+
+    def wrapped_func(pairs: Sequence[tuple[str, str]]) -> SimSeq:
+        texts = _unique_items(pairs)
+        res = client.embeddings.create(input=texts, model=model_name)
+        _vecs = [np.array(x.embedding) for x in res.data]
+        vecs = dict(zip(texts, _vecs, strict=True))
+
+        return [_cosine(vecs[x], vecs[y]) for x, y in pairs]
 
     return wrapped_func
 
