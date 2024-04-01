53bb48186970c8f9e6e10df0ab3e62ee2216d7a1
Martin-Hoppe <63357467+Martin-Hoppe@users.noreply.github.com>
2024-04-01 21:13:01 +0100

    feat: add similarity measures for time-series data (#80)
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    Co-authored-by: Mirko Lenz <mirko@mirkolenz.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- cbrkit/sim/collections.py
+++ cbrkit/sim/collections.py
@@ -1,10 +1,12 @@
-from collections.abc import Collection, Set
+from collections.abc import Collection, Sequence, Set
 from typing import Any
 
 from cbrkit.helpers import dist2sim
 from cbrkit.typing import SimPairFunc
 
-__all__ = ["jaccard"]
+Number = float | int
+
+__all__ = ["jaccard", "smith_waterman", "dtw"]
 
 
 def jaccard() -> SimPairFunc[Collection[Any], float]:
@@ -24,5 +26,66 @@ def jaccard() -> SimPairFunc[Collection[Any], float]:
             y = set(y)
 
         return dist2sim(jaccard_distance(x, y))
+
+    return wrapped_func
+
+
+def smith_waterman(
+    match_score: int = 2, mismatch_penalty: int = -1, gap_penalty: int = -1
+) -> SimPairFunc[Sequence[Any], float]:
+    """
+    Performs the Smith-Waterman alignment with configurable scoring parameters. If no element matches it returns 0.0.
+
+    Args:
+        match_score: Score for matching characters. Defaults to 2.
+        mismatch_penalty: Penalty for mismatching characters. Defaults to -1.
+        gap_penalty: Penalty for gaps. Defaults to -1.
+
+    Example:
+        >>> sim = smith_waterman()
+        >>> sim("abcde", "fghe")
+        2
+    """
+    from minineedle import core, smith
+
+    def wrapped_func(x: Sequence[Any], y: Sequence[Any]) -> float:
+        try:
+            alignment = smith.SmithWaterman(x, y)
+            alignment.change_matrix(
+                core.ScoreMatrix(
+                    match=match_score, miss=mismatch_penalty, gap=gap_penalty
+                )
+            )
+            alignment.align()
+
+            return alignment.get_score()
+        except ZeroDivisionError:
+            return 0.0
+
+    return wrapped_func
+
+
+def dtw() -> SimPairFunc[Collection[int], float]:
+    """Dynamic Time Warping similarity function.
+
+    Examples:
+        >>> sim = dtw()
+        >>> sim([1, 2, 3], [1, 2, 3, 4])
+        0.5
+    """
+    import numpy as np
+    from dtaidistance import dtw
+
+    def wrapped_func(
+        x: Collection[Number] | np.ndarray, y: Collection[Number] | np.ndarray
+    ) -> float:
+        if not isinstance(x, np.ndarray):
+            x = np.array(x)
+        if not isinstance(y, np.ndarray):
+            y = np.array(y)
+
+        distance = dtw.distance(x, y)
+
+        return dist2sim(distance)
 
     return wrapped_func
