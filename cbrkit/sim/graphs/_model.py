3b419f2184639992cf160f733c18fd42da051089
Martin Hoppe <Martin.Hoppe@hochschule-stralsund.de>
2024-11-04 18:03:51 +0100

    add networkx (note: untested yet)

--- cbrkit/sim/graphs/_model.py
+++ cbrkit/sim/graphs/_model.py
@@ -46,9 +46,9 @@ class Node[K, N](HasData[N]):
 
     @classmethod
     def from_dict(
-        cls,
-        key: K,
-        data: SerializedNode[N],
+            cls,
+            key: K,
+            data: SerializedNode[N],
     ) -> "Node[K, N]":
         return cls(key, data["data"])
 
@@ -69,10 +69,10 @@ class Edge[K, N, E](HasData[E]):
 
     @classmethod
     def from_dict(
-        cls,
-        key: K,
-        data: SerializedEdge[K, E],
-        nodes: Mapping[K, Node[K, N]],
+            cls,
+            key: K,
+            data: SerializedEdge[K, E],
+            nodes: Mapping[K, Node[K, N]],
     ) -> "Edge[K, N, E]":
         return cls(
             key,
@@ -97,8 +97,8 @@ class Graph[K, N, E, G](HasData[G]):
 
     @classmethod
     def from_dict(
-        cls,
-        g: SerializedGraph[K, N, E, G],
+            cls,
+            g: SerializedGraph[K, N, E, G],
     ) -> "Graph[K, N, E, G]":
         nodes = immutables.Map(
             (key, Node.from_dict(key, value)) for key, value in g["nodes"].items()
@@ -110,53 +110,113 @@ class Graph[K, N, E, G](HasData[G]):
         return cls(nodes, edges, g["data"])
 
 
-def to_dict[K, N, E, G](g: Graph[K, N, E, G]) -> SerializedGraph[K, N, E, G]:
-    return g.to_dict()
+def to_dict[K, N, E, G](g: Graph[K, N, E, G]
+
+) -> SerializedGraph[K, N, E, G]:
+return g.to_dict()
 
 
-def from_dict[K, N, E, G](g: SerializedGraph[K, N, E, G]) -> Graph[K, N, E, G]:
-    return Graph.from_dict(g)
+def from_dict[K, N, E, G](g: SerializedGraph[K, N, E, G]
 
+) -> Graph[K, N, E, G]:
+return Graph.from_dict(g)
 
 try:
     import rustworkx
 
-    def to_rustworkx[N, E](g: Graph[Any, N, E, Any]) -> "rustworkx.PyDiGraph[N, E]":
-        ng = rustworkx.PyDiGraph(attrs=g.data)
-        new_ids = ng.add_nodes_from(list(g.nodes.values()))
-        id_map = {
-            old_id: new_id
-            for old_id, new_id in zip(g.nodes.keys(), new_ids, strict=True)
-        }
-        ng.add_edges_from(
-            [
-                (
-                    id_map[edge.source.key],
-                    id_map[edge.target.key],
-                    edge.data,
-                )
-                for edge in g.edges.values()
-            ]
-        )
 
-        return ng
+    def to_rustworkx[N, E](g: Graph[Any, N, E, Any]
 
-    def from_rustworkx[N, E](g: "rustworkx.PyDiGraph[N, E]") -> Graph[int, N, E, Any]:
-        nodes = immutables.Map(
-            (idx, Node(idx, g.get_node_data(idx))) for idx in g.node_indices()
-        )
-        edges = immutables.Map(
-            (edge_id, Edge(edge_id, nodes[source_id], nodes[target_id], edge_data))
-            for edge_id, (
-                source_id,
-                target_id,
-                edge_data,
-            ) in g.edge_index_map().items()
-        )
+    ) -> "rustworkx.PyDiGraph[N, E]":
+    ng = rustworkx.PyDiGraph(attrs=g.data)
+    new_ids = ng.add_nodes_from(list(g.nodes.values()))
+    id_map = {
+        old_id: new_id
+        for old_id, new_id in zip(g.nodes.keys(), new_ids, strict=True)
+    }
+    ng.add_edges_from(
+        [
+            (
+                id_map[edge.source.key],
+                id_map[edge.target.key],
+                edge.data,
+            )
+            for edge in g.edges.values()
+        ]
+    )
 
-        return Graph(nodes, edges, g.attrs)
+    return ng
 
-    __all__ += ["to_rustworkx", "from_rustworkx"]
+
+def from_rustworkx[N, E](g: "rustworkx.PyDiGraph[N, E]"
+
+) -> Graph[int, N, E, Any]:
+nodes = immutables.Map(
+    (idx, Node(idx, g.get_node_data(idx))) for idx in g.node_indices()
+)
+edges = immutables.Map(
+    (edge_id, Edge(edge_id, nodes[source_id], nodes[target_id], edge_data))
+    for edge_id, (
+        source_id,
+        target_id,
+        edge_data,
+    ) in g.edge_index_map().items()
+)
+
+return Graph(nodes, edges, g.attrs)
+
+__all__ += ["to_rustworkx", "from_rustworkx"]
 
 except ImportError:
-    pass
+pass
+
+try:
+    import networkx as nx
+
+
+    def to_networkx[N, E](g: Graph[Any, N, E, Any]
+
+    ) -> "nx.DiGraph":
+    ng = nx.DiGraph()
+    # Set graph attributes
+    ng.graph.update(g.data)
+
+    # Add nodes with their data
+    for node in g.nodes.values():
+        ng.add_node(node.key, data=node.data)
+
+    # Add edges with their data
+    for edge in g.edges.values():
+        ng.add_edge(edge.source.key, edge.target.key,
+                    key=edge.key, data=edge.data)
+
+    return ng
+
+
+def from_networkx[N, E](g: "nx.DiGraph"
+
+) -> Graph[Any, N, E, Any]:
+# Create nodes
+nodes = immutables.Map(
+    (node_id, Node(node_id, g.nodes[node_id].get('data')))
+    for node_id in g.nodes
+)
+
+# Create edges
+edges = immutables.Map(
+    (edge_data.get('key', idx),
+     Edge(
+         edge_data.get('key', idx),
+         nodes[source],
+         nodes[target],
+         edge_data.get('data')
+     ))
+    for idx, (source, target, edge_data) in enumerate(g.edges(data=True))
+)
+
+return Graph(nodes, edges, dict(g.graph))
+
+__all__ += ["to_networkx", "from_networkx"]
+
+except ImportError:
+pass
