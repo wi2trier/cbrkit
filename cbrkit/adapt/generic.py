c60b0d31fdeda78be4b6e598597412d15d468797
Mirko Lenz <mirko@mirkolenz.com>
2024-11-14 15:49:14 +0100

    fix(adapt): rename rules to pipe

--- cbrkit/adapt/generic.py
+++ cbrkit/adapt/generic.py
@@ -4,23 +4,27 @@ from typing import override
 from cbrkit.helpers import get_metadata
 from cbrkit.typing import AdaptPairFunc, JsonDict, SupportsMetadata
 
+__all__ = [
+    "pipe",
+]
+
 
 @dataclass(slots=True, frozen=True)
-class rules[V](AdaptPairFunc[V], SupportsMetadata):
-    rules: list[AdaptPairFunc[V]]
+class pipe[V](AdaptPairFunc[V], SupportsMetadata):
+    functions: list[AdaptPairFunc[V]]
 
     @property
     @override
     def metadata(self) -> JsonDict:
         return {
-            "rules": [get_metadata(rule) for rule in self.rules],
+            "functions": [get_metadata(func) for func in self.functions],
         }
 
     @override
     def __call__(self, case: V, query: V) -> V:
         current_case = case
 
-        for rule in self.rules:
-            current_case = rule(current_case, query)
+        for func in self.functions:
+            current_case = func(current_case, query)
 
         return current_case
