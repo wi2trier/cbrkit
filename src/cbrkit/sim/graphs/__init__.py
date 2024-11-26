4bb52ca42824f12bbf5f1d668f7c6f519c0289b9
Mirko Lenz <mirko@mirkolenz.com>
2024-11-26 15:48:54 +0100

    feat(sim/graphs): add initial version of exhaustive mapping

--- src/cbrkit/sim/graphs/__init__.py
+++ src/cbrkit/sim/graphs/__init__.py
@@ -1,9 +1,11 @@
 from . import _model as model
 from ._astar import astar
+from ._exhaustive import exhaustive
 from ._isomorphism import isomorphism
 
 __all__ = [
     "model",
     "astar",
+    "exhaustive",
     "isomorphism",
 ]
