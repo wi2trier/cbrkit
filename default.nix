03baf1de13163105bb96d1c2857c31b8f14bed49
Mirko Lenz <mirko@mirkolenz.com>
2024-12-29 23:37:01 +0100

    build: use pyproject-nix util

--- default.nix
+++ default.nix
@@ -13,8 +13,8 @@ let
   pdocRepo = fetchFromGitHub {
     owner = "mitmproxy";
     repo = "pdoc";
-    rev = "v15.0.0";
-    hash = "sha256-6XEcHhaKkxY/FU748f+OsTcSgrM4iQTmJAL8rJ3EqnY=";
+    tag = "v15.0.1";
+    hash = "sha256-HDrDGnK557EWbBQtsvDzTst3oV0NjLRm4ilXaxd6/j8=";
   };
   workspace = uv2nix.lib.workspace.loadWorkspace { workspaceRoot = ./.; };
   projectOverlay = workspace.mkPyprojectOverlay {
@@ -56,6 +56,14 @@ let
         buildInputs = (old.buildInputs or [ ]) ++ [ tbb_2021_11 ];
       };
       cbrkit = old: {
+        meta = (old.meta or { }) // {
+          mainProgram = "cbrkit";
+          maintainers = with lib.maintainers; [ mirkolenz ];
+          license = lib.licenses.mit;
+          homepage = "https://github.com/wi2trier/cbrkit";
+          description = "Generate entire directory structures using Jinja templates with support for external data and custom plugins.";
+          platforms = with lib.platforms; darwin ++ linux;
+        };
         passthru = lib.recursiveUpdate (old.passthru or { }) {
           tests.pytest = stdenv.mkDerivation {
             name = "${final.cbrkit.name}-pytest";
@@ -124,6 +132,10 @@ let
   baseSet = callPackage pyproject-nix.build.packages {
     python = python3;
   };
+in
+{
+  inherit workspace;
+  inherit (callPackage pyproject-nix.build.util { }) mkApplication;
   pythonSet = baseSet.overrideScope (
     lib.composeManyExtensions [
       pyproject-build-systems.overlays.default
@@ -133,29 +145,4 @@ let
       packageOverlay
     ]
   );
-  addMeta =
-    drv:
-    drv.overrideAttrs (old: {
-      passthru = lib.recursiveUpdate (old.passthru or { }) {
-        inherit (pythonSet.cbrkit.passthru) tests;
-      };
-      meta = (old.meta or { }) // {
-        mainProgram = "cbrkit";
-        maintainers = with lib.maintainers; [ mirkolenz ];
-        license = lib.licenses.mit;
-        homepage = "https://github.com/wi2trier/cbrkit";
-        description = "Generate entire directory structures using Jinja templates with support for external data and custom plugins.";
-        platforms = with lib.platforms; darwin ++ linux;
-      };
-    });
-in
-pythonSet
-// {
-  mkApp =
-    cbrkitSpec:
-    addMeta (
-      pythonSet.mkVirtualEnv "cbrkit-env" {
-        cbrkit = cbrkitSpec;
-      }
-    );
 }
