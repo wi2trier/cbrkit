866121b83bbca7885890232c7eb5ecd7f131efc1
Mirko Lenz <mirko@mirkolenz.com>
2025-01-16 23:38:07 +0100

    feat(sim): add default sim for attribute value

--- src/cbrkit/sim/attribute_value.py
+++ src/cbrkit/sim/attribute_value.py
@@ -32,6 +32,8 @@ class attribute_value[V, S: Float](BatchSimFunc[V, AttributeValueSim[S]]):
         attributes: A mapping of attribute names to the similarity functions to be used for those attributes.
         aggregator: A function that aggregates the local similarity scores for each attribute into a single global similarity.
         value_getter: A function that retrieves the value of an attribute from a case.
+        default: The default similarity score to use when an error occurs during the computation of a similarity score.
+            For example, if a case does not have an attribute that is required for the similarity computation.
 
     Examples:
         >>> equality = lambda x, y: 1.0 if x == y else 0.0
@@ -52,6 +54,7 @@ class attribute_value[V, S: Float](BatchSimFunc[V, AttributeValueSim[S]]):
     attributes: Mapping[str, AnySimFunc[Any, S]]
     aggregator: AggregatorFunc[str, S] = default_aggregator
     value_getter: Callable[[Any, str], Any] = getitem_or_getattr
+    default: S | None = None
 
     @override
     def __call__(self, batches: Sequence[tuple[V, V]]) -> SimSeq[AttributeValueSim[S]]:
@@ -62,14 +65,23 @@ class attribute_value[V, S: Float](BatchSimFunc[V, AttributeValueSim[S]]):
 
         for attr_name in self.attributes:
             logger.debug(f"Processing attribute {attr_name}")
-            attribute_values = [
-                (self.value_getter(x, attr_name), self.value_getter(y, attr_name))
-                for x, y in batches
-            ]
-            sim_func = batchify_sim(self.attributes[attr_name])
-            sim_func_result = sim_func(attribute_values)
 
-            for idx, sim in enumerate(sim_func_result):
-                local_sims[idx][attr_name] = sim
+            try:
+                attribute_values = [
+                    (self.value_getter(x, attr_name), self.value_getter(y, attr_name))
+                    for x, y in batches
+                ]
+                sim_func = batchify_sim(self.attributes[attr_name])
+                sim_func_result = sim_func(attribute_values)
+
+                for idx, sim in enumerate(sim_func_result):
+                    local_sims[idx][attr_name] = sim
+
+            except Exception as e:
+                if self.default is not None:
+                    for idx in range(len(batches)):
+                        local_sims[idx][attr_name] = self.default
+                else:
+                    raise e
 
         return [AttributeValueSim(self.aggregator(sims), sims) for sims in local_sims]
