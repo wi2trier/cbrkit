0fad3c5d36defd9483d9150ef38fe4cf1d625d4b
Mirko Lenz <mirko@mirkolenz.com>
2025-01-28 16:42:35 +0100

    feat(synthesis/providers): add delay parameter

--- src/cbrkit/synthesis/providers/model.py
+++ src/cbrkit/synthesis/providers/model.py
@@ -30,13 +30,25 @@ class DocumentsPrompt[P](StructuredValue[P]):
 class BaseProvider[P, R](BatchConversionFunc[P, R], ABC):
     model: str
     response_type: type[R]
+    delay: float = 0
     extra_kwargs: Mapping[str, Any] = field(default_factory=dict)
 
     def __call__(self, batches: Sequence[P]) -> Sequence[R]:
         return event_loop.get().run_until_complete(self.__call_batches__(batches))
 
     async def __call_batches__(self, batches: Sequence[P]) -> Sequence[R]:
-        return await asyncio.gather(*(self.__call_batch__(batch) for batch in batches))
+        return await asyncio.gather(
+            *(
+                self.__call_batch_with_delay__(batch, idx)
+                for idx, batch in enumerate(batches)
+            )
+        )
+
+    async def __call_batch_with_delay__(self, prompt: P, idx: int) -> R:
+        if self.delay > 0:
+            await asyncio.sleep(idx * self.delay)
+
+        return await self.__call_batch__(prompt)
 
     @abstractmethod
     async def __call_batch__(self, prompt: P) -> R: ...
