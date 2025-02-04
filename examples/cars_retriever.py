69979334f3b205128923b1cbbe627e72f17eefc7
Mirko Lenz <mirko@mirkolenz.com>
2025-02-04 16:54:54 +0100

    chore: update curl example

--- examples/cars_retriever.py
+++ examples/cars_retriever.py
@@ -1,6 +1,11 @@
 # uv run -m cbrkit retrieve data/cars-1k.csv data/cars-queries.csv examples.cars_retriever:retriever --output-path data/output.json
 # uv run -m cbrkit serve --retriever examples.cars_retriever:retriever
-# curl --location "localhost:8080/retrieve" --form casebase="/Users/mlenz/Developer/wi2trier/cbrkit/data/cars-1k.csv" --form queries="/Users/mlenz/Developer/wi2trier/cbrkit/data/cars-queries.csv"
+# curl --location 'localhost:8080/retrieve' \
+# --header 'Content-Type: application/json' \
+# --data '{
+#     "casebase": "PATH_TO_CASEBASE.csv",
+#     "queries": "PATH_TO_QUERIES.csv"
+# }'
 import cbrkit
 
 retriever = cbrkit.retrieval.dropout(
