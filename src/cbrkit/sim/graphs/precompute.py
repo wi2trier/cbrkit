e92660d41a679611ab8e1d774884f61a5232a2f2
Mirko Lenz <mirko@mirkolenz.com>
2025-05-23 22:31:39 +0200

    fix(sim/graphs): adapt precompute to new common logic

--- src/cbrkit/sim/graphs/precompute.py
+++ src/cbrkit/sim/graphs/precompute.py
@@ -1,57 +1,39 @@
 import itertools
 from collections.abc import Sequence
-from dataclasses import dataclass
+from dataclasses import InitVar, dataclass, field
 
-from ...helpers import (
-    batchify_sim,
-)
+from ...helpers import batchify_sim
 from ...model.graph import (
-    Edge,
     Graph,
     Node,
 )
 from ...typing import AnySimFunc, BatchSimFunc, Float
 from ..wrappers import transpose_value
+from .common import ElementMatcher
 
 
-@dataclass(slots=True, frozen=True)
+@dataclass(slots=True)
 class precompute[K, N, E, G](BatchSimFunc[Graph[K, N, E, G], float]):
-    node_sim_func: AnySimFunc[N, Float] | None = None
-    edge_sim_func: AnySimFunc[Edge[K, N, E], Float] | None = None
+    node_sim_func: InitVar[AnySimFunc[N, Float]]
+    node_matcher: ElementMatcher[N]
+    batch_node_sim_func: BatchSimFunc[Node[K, N], Float] = field(init=False)
+    # TODO: Add support for precomputing edges
+
+    def __post_init__(self, any_node_sim_func: AnySimFunc[N, Float]) -> None:
+        self.batch_node_sim_func = batchify_sim(transpose_value(any_node_sim_func))
 
     def __call__(
         self, batches: Sequence[tuple[Graph[K, N, E, G], Graph[K, N, E, G]]]
     ) -> list[float]:
-        if self.node_sim_func is not None:
-            node_sim_func = batchify_sim(transpose_value(self.node_sim_func))
-            node_batches: list[tuple[Node[K, N], Node[K, N]]] = []
+        node_pairs: list[tuple[Node[K, N], Node[K, N]]] = []
 
-            for x, y in batches:
-                node_batches.extend(
-                    [
-                        (x.nodes[x_key], y.nodes[y_key])
-                        for x_key, y_key in itertools.product(
-                            x.nodes.keys(), y.nodes.keys()
-                        )
-                    ]
-                )
+        for x, y in batches:
+            node_pairs.extend(
+                (x.nodes[x_key], y.nodes[y_key])
+                for x_key, y_key in itertools.product(x.nodes.keys(), y.nodes.keys())
+                if self.node_matcher(x.nodes[x_key].value, y.nodes[y_key].value)
+            )
 
-            node_sim_func(node_batches)
-
-        if self.edge_sim_func is not None:
-            edge_sim_func = batchify_sim(self.edge_sim_func)
-            edge_batches: list[tuple[Edge[K, N, E], Edge[K, N, E]]] = []
-
-            for x, y in batches:
-                edge_batches.extend(
-                    [
-                        (x.edges[x_key], y.edges[y_key])
-                        for x_key, y_key in itertools.product(
-                            x.edges.keys(), y.edges.keys()
-                        )
-                    ]
-                )
-
-            edge_sim_func(edge_batches)
+        self.batch_node_sim_func(node_pairs)
 
         return [1.0] * len(batches)
