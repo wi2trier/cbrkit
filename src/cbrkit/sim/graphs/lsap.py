b2285f6bca4ae8316f7013760d8725a42c34260e
Mirko Lenz <mirko@mirkolenz.com>
2025-06-01 17:45:23 +0200

    fix(sim/graphs/lsap): handle value errors properly

--- src/cbrkit/sim/graphs/lsap.py
+++ src/cbrkit/sim/graphs/lsap.py
@@ -38,49 +38,63 @@ class lsap[K, N, E, G](
         idx2y = {idx: y_key for idx, y_key in enumerate(y.nodes.keys())}
         idx2x = {idx: x_key for idx, x_key in enumerate(x.nodes.keys())}
 
-        if self.variant == "dense":
-            cost = np.array(
-                [
+        try:
+            if self.variant == "dense":
+                cost = np.array(
                     [
-                        1.0 - node_pair_sims[(y_key, x_key)]
-                        if (y_key, x_key) in node_pair_sims
-                        else self.illegal_mapping_cost
-                        for x_key in x.nodes.keys()
+                        [
+                            1.0 - node_pair_sims[(y_key, x_key)]
+                            if (y_key, x_key) in node_pair_sims
+                            else self.illegal_mapping_cost
+                            for x_key in x.nodes.keys()
+                        ]
+                        for y_key in y.nodes.keys()
                     ]
-                    for y_key in y.nodes.keys()
-                ]
-            )
-            row_indexes, col_indexes = linear_sum_assignment(cost)
-            node_mapping = frozendict(
-                (idx2y[row_idx], idx2x[col_idx])
-                for row_idx, col_idx in zip(row_indexes, col_indexes, strict=True)
-                if cost[row_idx, col_idx] < self.illegal_mapping_cost
-            )
+                )
+                row_indexes, col_indexes = linear_sum_assignment(cost)
+                node_mapping = frozendict(
+                    (idx2y[row_idx], idx2x[col_idx])
+                    for row_idx, col_idx in zip(row_indexes, col_indexes, strict=True)
+                    if cost[row_idx, col_idx] < self.illegal_mapping_cost
+                )
 
-        elif self.variant == "sparse":
-            cost = np.array(
-                [
+            elif self.variant == "sparse":
+                cost = np.array(
                     [
-                        # From the documentation:
-                        # We require that weights are non-zero only to avoid issues with the handling of explicit zeros when converting between different sparse representations.
-                        # Zero weights can be handled by adding a constant to all weights, so that the resulting matrix contains no zeros.
-                        2.0 - node_pair_sims[(y_key, x_key)]
-                        if (y_key, x_key) in node_pair_sims
-                        else 0.0
-                        for x_key in x.nodes.keys()
+                        [
+                            # From the documentation:
+                            # We require that weights are non-zero only to avoid issues with the handling of explicit zeros when converting between different sparse representations.
+                            # Zero weights can be handled by adding a constant to all weights, so that the resulting matrix contains no zeros.
+                            2.0 - node_pair_sims[(y_key, x_key)]
+                            if (y_key, x_key) in node_pair_sims
+                            else 0.0
+                            for x_key in x.nodes.keys()
+                        ]
+                        for y_key in y.nodes.keys()
                     ]
-                    for y_key in y.nodes.keys()
-                ]
-            )
-            biadjacency = csr_array(cost)
-            row_indexes, col_indexes = min_weight_full_bipartite_matching(biadjacency)
-            node_mapping = frozendict(
-                (idx2y[row_idx], idx2x[col_idx])
-                for row_idx, col_idx in zip(row_indexes, col_indexes, strict=True)
-            )
+                )
+                biadjacency = csr_array(cost)
+                row_indexes, col_indexes = min_weight_full_bipartite_matching(
+                    biadjacency
+                )
+                node_mapping = frozendict(
+                    (idx2y[row_idx], idx2x[col_idx])
+                    for row_idx, col_idx in zip(row_indexes, col_indexes, strict=True)
+                )
 
-        else:
-            raise ValueError(f"Invalid variant '{self.variant}'.")
+            else:
+                raise ValueError(f"Invalid variant '{self.variant}'.")
+
+        except ValueError as e:
+            logger.warning(f"Failed to compute LSAP mapping for two graphs: {e}")
+
+            return GraphSim(
+                0.0,
+                frozendict(),
+                frozendict(),
+                frozendict(),
+                frozendict(),
+            )
 
         edge_mapping = self.induced_edge_mapping(x, y, node_mapping)
 
