fbc6205d68771ebb87434d9365f6249f3642a988
Mirko Lenz <mirko@mirkolenz.com>
2025-08-28 08:44:41 +0200

    fix(system): rework integrations to be more integrated

--- src/cbrkit/system.py
+++ src/cbrkit/system.py
@@ -1,8 +1,8 @@
 from collections.abc import Callable
 from dataclasses import dataclass
+from typing import Annotated, Any
 
 from pydantic import BaseModel
-from typing_extensions import Any
 
 import cbrkit
 from cbrkit.typing import Float, MaybeSequence
@@ -20,8 +20,8 @@ class System[
     K: str | int,
     V: BaseModel,
     S: Float,
-    R2: BaseModel | None,
     R1: BaseModel | None,
+    R2: BaseModel | None,
 ]:
     casebase: cbrkit.typing.Casebase[K, V]
     retriever_factory: (
@@ -34,7 +34,7 @@ class System[
     def retrieve(
         self,
         query: V,
-        parameters: R1,
+        config: R1,
     ) -> cbrkit.retrieval.QueryResultStep[K, V, S]:
         if not self.retriever_factory:
             raise ValueError("Retriever factory is not defined.")
@@ -42,13 +42,13 @@ class System[
         return cbrkit.retrieval.apply_query(
             self.casebase,
             query,
-            self.retriever_factory(parameters),
+            self.retriever_factory(config),
         ).default_query
 
     def reuse(
         self,
         query: V,
-        parameters: R2,
+        config: R2,
     ) -> cbrkit.retrieval.QueryResultStep[K, V, S]:
         if not self.reuser_factory:
             raise ValueError("Reuser factory is not defined.")
@@ -56,14 +56,14 @@ class System[
         return cbrkit.reuse.apply_query(
             self.casebase,
             query,
-            self.reuser_factory(parameters),
+            self.reuser_factory(config),
         ).default_query
 
     def cycle(
         self,
         query: V,
-        retrieve_parameters: R1,
-        reuse_parameters: R2,
+        retrieve_config: R1,
+        reuse_config: R2,
     ) -> cbrkit.retrieval.QueryResultStep[K, V, S]:
         if not self.retriever_factory or not self.reuser_factory:
             raise ValueError("Retriever or reuser factory is not defined.")
@@ -71,51 +71,74 @@ class System[
         return cbrkit.cycle.apply_query(
             self.casebase,
             query,
-            self.retriever_factory(retrieve_parameters),
-            self.reuser_factory(reuse_parameters),
+            self.retriever_factory(retrieve_config),
+            self.reuser_factory(reuse_config),
         ).final_step.default_query
-
-    @property
-    def tools(self) -> list[Callable[..., Any]]:
-        res: list[Callable[..., Any]] = []
-
-        if self.retriever_factory:
-            res.append(self.retrieve)
-
-        if self.reuser_factory:
-            res.append(self.reuse)
-
-        if self.retriever_factory and self.reuser_factory:
-            res.append(self.cycle)
-
-        return res
-
-    def get_case(self, name: K) -> V:
-        return self.casebase[name]
-
-    @property
-    def resources(self) -> dict[str, Callable[..., Any]]:
-        return {
-            "casebase://{name}": self.get_case,
-        }
-
-    @property
-    def prompts(self) -> list[Callable[..., Any]]:
-        return []
 
 
 with cbrkit.helpers.optional_dependencies():
-    from fastapi import APIRouter, FastAPI
+    from fastapi import APIRouter, Body, FastAPI
 
-    def to_fastapi[T: APIRouter | FastAPI](system: System, app: T) -> T:
-        for value in system.tools:
-            app.post(f"/tool/{value.__name__}")(value)
+    @dataclass(slots=True, frozen=True)
+    class FastAPISystem[
+        K: str | int,
+        V: BaseModel,
+        S: Float,
+        R1: BaseModel | None,
+        R2: BaseModel | None,
+    ]:
+        system: System[K, V, S, R1, R2]
 
-        for key, value in system.resources.items():
-            app.get(f"/resource/{key.replace('://', '/')}")(value)
+        def retrieve(
+            self,
+            query: Annotated[V, Body],
+            config: R1,
+        ) -> cbrkit.retrieval.QueryResultStep[K, V, S]:
+            return self.system.retrieve(query, config)
 
-        for value in system.prompts:
-            app.post(f"/prompt/{value.__name__}")(value)
+        def reuse(
+            self,
+            query: Annotated[V, Body],
+            config: R2,
+        ) -> cbrkit.retrieval.QueryResultStep[K, V, S]:
+            return self.system.reuse(query, config)
+
+        def cycle(
+            self,
+            query: Annotated[V, Body],
+            retrieve_config: R1,
+            reuse_config: R2,
+        ) -> cbrkit.retrieval.QueryResultStep[K, V, S]:
+            return self.system.cycle(query, retrieve_config, reuse_config)
+
+        def casebase(self) -> cbrkit.typing.Casebase[K, V]:
+            return self.system.casebase
+
+        def case(self, key: K) -> V | None:
+            return self.system.casebase.get(key)
+
+    def to_fastapi[
+        K: str | int,
+        V: BaseModel,
+        S: Float,
+        R1: BaseModel | None,
+        R2: BaseModel | None,
+        T: APIRouter | FastAPI,
+    ](system: System[K, V, S, R1, R2], app: T) -> T:
+        fastapi_system = FastAPISystem(system)
+
+        if system.retriever_factory:
+            app.post("/retrieve")(fastapi_system.retrieve)
+
+        if system.reuser_factory:
+            app.post("/reuse")(fastapi_system.reuse)
+
+        if system.retriever_factory and system.reuser_factory:
+            app.post("/cycle")(fastapi_system.cycle)
+
+        app.get("/casebase")(fastapi_system.casebase)
+
+        app.get("/casebase/{key}")(fastapi_system.case)
 
         return app
 
@@ -123,15 +146,64 @@ with cbrkit.helpers.optional_dependencies():
 with cbrkit.helpers.optional_dependencies():
     from fastmcp import FastMCP
 
-    def to_fastmcp[T: FastMCP](system: System, app: T) -> T:
-        for value in system.tools:
-            app.tool(value)
+    @dataclass(slots=True, frozen=True)
+    class FastMCPSystem[
+        K: str | int,
+        V: BaseModel,
+        S: Float,
+        R1: BaseModel | None,
+        R2: BaseModel | None,
+    ]:
+        system: System[K, V, S, R1, R2]
 
-        for key, value in system.resources.items():
-            app.resource(key)(value)
+        def retrieve(
+            self,
+            query: V,
+            config: R1,
+        ) -> cbrkit.retrieval.QueryResultStep[K, V, S]:
+            return self.system.retrieve(query, config)
 
-        for value in system.prompts:
-            app.prompt(value)
+        def reuse(
+            self,
+            query: V,
+            config: R2,
+        ) -> cbrkit.retrieval.QueryResultStep[K, V, S]:
+            return self.system.reuse(query, config)
+
+        def cycle(
+            self,
+            query: V,
+            retrieve_config: R1,
+            reuse_config: R2,
+        ) -> cbrkit.retrieval.QueryResultStep[K, V, S]:
+            return self.system.cycle(query, retrieve_config, reuse_config)
+
+        def casebase(self) -> cbrkit.typing.Casebase[K, V]:
+            return self.system.casebase
+
+        def case(self, key: K) -> V | None:
+            return self.system.casebase.get(key)
+
+    def to_fastmcp[
+        K: str | int,
+        V: BaseModel,
+        S: Float,
+        R1: BaseModel | None,
+        R2: BaseModel | None,
+        T: FastMCP,
+    ](system: System[K, V, S, R1, R2], app: T) -> T:
+        fastmcp_system = FastMCPSystem(system)
+
+        if system.retriever_factory:
+            app.tool("/retrieve")(fastmcp_system.retrieve)
+
+        if system.reuser_factory:
+            app.tool("/reuse")(fastmcp_system.reuse)
+
+        if system.retriever_factory and system.reuser_factory:
+            app.tool("/cycle")(fastmcp_system.cycle)
+
+        app.resource("casebase://{key}")(fastmcp_system.case)
 
         return app
 
@@ -140,4 +212,15 @@ with cbrkit.helpers.optional_dependencies():
     from pydantic_ai.toolsets import FunctionToolset
 
     def to_pydantic_ai(system: System) -> FunctionToolset[None]:
-        return FunctionToolset(system.tools)
+        tools: list[Callable[..., Any]] = []
+
+        if system.retriever_factory:
+            tools.append(system.retrieve)
+
+        if system.reuser_factory:
+            tools.append(system.reuse)
+
+        if system.retriever_factory and system.reuser_factory:
+            tools.append(system.cycle)
+
+        return FunctionToolset(tools)
